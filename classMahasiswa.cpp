2de389b0ee187c156e9ce0db83637652c43cece8
BimoFr <bimofadhlurrahman@gmail.com>
2026-04-28 09:33:02 +0700

    add printData function

--- classMahasiswa.cpp
+++ classMahasiswa.cpp
@@ -4,8 +4,15 @@ using namespace std;
 
 class Mahasiswa
 {
-    public:
-        int NIM;
-        string nama;
-        float nilai;
+public:
+    int NIM;
+    string nama;
+    float nilai;
+
+    void printData()
+    {
+        cout << "NIM: " << NIM << endl;
+        cout << "Nama: " << nama << endl;
+        cout << "Nilai: " << nilai << endl;
+    }
 };
